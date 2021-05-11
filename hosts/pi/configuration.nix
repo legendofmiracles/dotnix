1b96dc5be1672a4589967e1b7f4ba15522b4dbb2
legendofmiracles <legendofmiracles@protonmail.com>
2021-05-11 09:27:54 +0200

    first steps for the pi

---
+++ hosts/pi/configuration.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, lib, ... }:
+{
+  # NixOS wants to enable GRUB by default
+  boot.loader.grub.enable = false;
+  # Enables the generation of /boot/extlinux/extlinux.conf
+  boot.loader.generic-extlinux-compatible.enable = true;
+
+  #  !!! If your board is a Raspberry Pi 3, select not latest (5.8 at the time)
+  #  !!! as it is currently broken (see https://github.com/NixOS/nixpkgs/issues/97064)
+  boot.kernelPackages = pkgs.linuxPackages;
+
+  # !!! This is only for ARMv6 / ARMv7. Don't enable this on AArch64, cache.nixos.org works there.
+  #nix.binaryCaches = lib.mkForce [ "https://app.cachix.org/cache/thefloweringash-armv7" ];
+  #nix.binaryCachePublicKeys = [ "thefloweringash-armv7.cachix.org-1:v+5yzBD2odFKeXbmC+OPWVqx4WVoIVO6UXgnSAWFtso=" ];
+
+  # !!! Needed for the virtual console to work on the RPi 3, as the default of 16M doesn't seem to be enough.
+  # If X.org behaves weirdly (I only saw the cursor) then try increasing this to 256M.
+  # On a Raspberry Pi 4 with 4 GB, you should either disable this parameter or increase to at least 64M if you want the USB ports to work.
+  boot.kernelParams = ["cma=32M"];
+
+  # File systems configuration for using the installer's partition layout
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/NIXOS_SD";
+      fsType = "ext4";
+    };
+  };
+
+  # !!! Adding a swap file is optional, but strongly recommended!
+  # swapDevices = [ { device = "/swapfile"; size = 1024; } ];
+}
