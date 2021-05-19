b5997626d6ecea192e547bfe96805f3f73c3fe74
legendofmiracles <legendofmiracles@protonmail.com>
2021-05-19 00:53:32 -0600

    adds comments

--- network.nix
+++ network.nix
@@ -7,8 +7,11 @@
     };
     useDHCP = false;
     interfaces = {
+      # normal computer
       enp8s0.useDHCP = true;
       wlp0s20f3.useDHCP = true;
+
+      # pi
       eth0.useDHCP = true;
     };
     nameservers = [ "1.1.1.1" ];
