c65b7ffccbeb22c4838c1d0e06b7c8d8a7c40c45
legendofmiracles <legendofmiracles@protonmail.com>
2021-06-25 11:27:28 -0600

    proton

--- HM/proton.nix
+++ HM/proton.nix
@@ -2,15 +2,16 @@
 
 let
   version = "6.10-GE-1";
-  source = fetchTarball {
+  source = fetch Tarball {
     url =
-      "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/${version}/Proton-${version}.tar.gz";
+      "https://github.com/Glorious Eggroll/proton-ge-custom/releases/download/${version}/Proton-${version}.tar.gz";
     sha256 = "sha256:0yibri0x6a4xxd8djavlsfap00gnf506r25cxpv50yglvi1a3rr1";
   };
 in {
-  home.file.proton-ge-custom = {
+  home.file.proton-ge-custom = rec {
     inherit source;
     recursive = true;
     target = ".steam/root/compatibilitytools.d/Proton-${version}/";
+    on Change = "mkdir ${target}/files/share/default_pfx/dosdevices";
   };
 }
