698ec95cfaa9d77b71debddd99fe763b0f6195bb
legendofmiracles <legendofmiracles@protonmail.com>
2021-09-17 22:58:01 -0600

    pi works now!

--- hosts/pi/configuration.nix
+++ hosts/pi/configuration.nix
@@ -4,14 +4,6 @@
   # Enables the generation of /boot/extlinux/extlinux.conf
   boot.loader.generic-extlinux-compatible.enable = true;
 
-  #  !!! If your board is a Raspberry Pi 3, select not latest (5.8 at the time)
-  #  !!! as it is currently broken (see https://github.com/NixOS/nixpkgs/issues/97064)
-  #boot.kernelPackages = pkgs.linuxPackages_5_4;
-
-  # !!! This is only for ARMv6 / ARMv7. Don't enable this on AArch64, cache.nixos.org works there.
-  #nix.binaryCaches = lib.mkForce [ "https://app.cachix.org/cache/thefloweringash-armv7" ];
-  #nix.binaryCachePublicKeys = [ "thefloweringash-armv7.cachix.org-1:v+5yzBD2odFKeXbmC+OPWVqx4WVoIVO6UXgnSAWFtso=" ];
-
   # !!! Needed for the virtual console to work on the RPi 3, as the default of 16M doesn't seem to be enough.
   # If X.org behaves weirdly (I only saw the cursor) then try increasing this to 256M.
   # On a Raspberry Pi 4 with 4 GB, you should either disable this parameter or increase to at least 64M if you want the USB ports to work.
@@ -26,7 +18,10 @@
   };
 
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    permitRootLogin = "yes";
+  };
 
   # Or disable the firewall altogether.
   networking.firewall.enable = false;
